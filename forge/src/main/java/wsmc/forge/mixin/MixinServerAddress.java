708e43e7510a9f8467c70f09e70ad2aa13fdb5d8
rikka0w0 <929513338@qq.com>
2023-10-06 03:52:55 +1100

    Patch ServerAddress to support WebSocket Uri parsing

---
+++ forge/src/main/java/wsmc/forge/mixin/MixinServerAddress.java
@@ -0,0 +1,116 @@
+package wsmc.forge.mixin;
+
+import org.spongepowered.asm.mixin.Debug;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import com.google.common.base.Objects;
+import com.google.common.net.HostAndPort;
+
+import net.minecraft.client.multiplayer.resolver.ServerAddress;
+import wsmc.IWebSocketServerAddress;
+
+@Debug(export = true)
+@Mixin(ServerAddress.class)
+public class MixinServerAddress implements IWebSocketServerAddress {
+	@Shadow
+	@Final
+	private HostAndPort hostAndPort;
+
+	@Unique
+	private String scheme = null;
+
+	@Unique
+	private String path = null;
+
+	@Override
+	public void setSchemeAndPath(String scheme, String path) {
+		this.path = path;
+		this.scheme = scheme;
+	}
+
+	@Override
+	public String getPath() {
+		return this.path;
+	}
+
+	@Override
+	public String getScheme() {
+		return this.scheme;
+	}
+
+	@Inject(at = @At("HEAD"), method = "toString", cancellable = true)
+	private void toStringCustom(CallbackInfoReturnable<String> callback) {
+		if (this.scheme != null) {
+			// Skip vanilla logic if scheme is non-null
+			String hostName = this.asServerAddress().getHost();
+			int port = this.asServerAddress().getPort();
+			callback.setReturnValue(this.scheme + "://" + hostName + ":" + port + this.path);
+		}
+	}
+
+	@Inject(at = @At("HEAD"), method = "equals", cancellable = true)
+	private void equalsCustom(Object object, CallbackInfoReturnable<Boolean> callback) {
+		// Redirects the vanilla equals();
+
+		if (this == object)
+			callback.setReturnValue(true);
+
+		if (this.scheme == null) {
+			// We are vanilla
+
+			if (object instanceof IWebSocketServerAddress) {
+				IWebSocketServerAddress ws = (IWebSocketServerAddress) object;
+
+				// We are vanilla but input is modded
+				if (ws.getScheme() != null)
+					callback.setReturnValue(false);
+			}
+
+			// Let vanilla code check for hostAndPort
+			return;
+		} else {
+			// We are modded
+
+			if (object instanceof IWebSocketServerAddress) {
+				IWebSocketServerAddress ws = (IWebSocketServerAddress) object;
+
+				// Scheme and path must equal
+				if (!ws.getScheme().equalsIgnoreCase(scheme) || !ws.getPath().equalsIgnoreCase(path))
+					callback.setReturnValue(false);
+
+				// Let vanilla code check for hostAndPort
+				return;
+			}
+
+			callback.setReturnValue(false);
+		}
+	}
+
+	@Inject(at = @At("HEAD"), method = "hashCode", cancellable = true)
+	private void hashCodeCustom(CallbackInfoReturnable<Integer> callback) {
+		if (this.scheme != null) {
+			// We are modded, cover scheme and path in the hash
+			int hash = Objects.hashCode(this.hostAndPort, this.scheme, this.path);
+			callback.setReturnValue(hash);
+		}
+	}
+
+	@Inject(at = @At("HEAD"), method = "parseString", cancellable = true)
+	private static void parseString(String uriString, CallbackInfoReturnable<ServerAddress> callback) {
+		ServerAddress serverAddress = IWebSocketServerAddress.fromWsUri(uriString);
+		if (serverAddress != null)
+			callback.setReturnValue(serverAddress);
+	}
+
+	@Inject(at = @At("HEAD"), method = "isValidAddress", cancellable = true)
+	private static void isValidAddress(String uriString, CallbackInfoReturnable<Boolean> callback) {
+		if (IWebSocketServerAddress.fromWsUri(uriString) != null)
+			callback.setReturnValue(true);
+	}
+}
---
+++ src/main/java/wsmc/IWebSocketServerAddress.java
@@ -0,0 +1,77 @@
+package wsmc;
+
+import java.net.IDN;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import javax.annotation.Nullable;
+
+import org.checkerframework.checker.nullness.qual.NonNull;
+
+import net.minecraft.client.multiplayer.resolver.ServerAddress;
+
+/**
+ * This will be mixined into vanilla class {@link ServerAddress}.
+ * Assumes only {@link ServerAddress} implements this interface.
+ */
+public interface IWebSocketServerAddress {
+	void setSchemeAndPath(@Nullable String scheme, @NonNull String path);
+
+	@NonNull
+	String getPath();
+
+	@Nullable
+	String getScheme();
+
+	default ServerAddress asServerAddress() {
+		return (ServerAddress) (Object) this;
+	}
+
+	/**
+	 *
+	 * @param uriString
+	 * @return null if uriString is not a valid WebSocket Uri (including vanilla TCP).
+	 */
+	@Nullable
+	public static ServerAddress fromWsUri(String uriString) {
+		try {
+			URI uri = new URI(uriString);
+
+			String scheme = uri.getScheme();
+			String hostname = uri.getHost();
+
+			if (hostname == null)
+				return null;
+
+			IDN.toASCII(hostname);
+
+			if (scheme == null)
+				return null;
+
+			// If the scheme is null, treat as vanilla TCP connection.
+			// If the scheme is ws or wss, treat as WebSocket connection.
+			// Otherwise, unsupported.
+
+			if (!scheme.equalsIgnoreCase("ws") && !scheme.equalsIgnoreCase("wss"))
+				return null;
+
+			int port = uri.getPort();
+			if (port < 0 || port > 65535) {
+				// Default port
+				port = 25565;
+			}
+
+			String path = uri.getPath();
+			if (path == null) {
+				path = "/";
+			}
+
+			ServerAddress result = new ServerAddress(hostname, port);
+			((IWebSocketServerAddress)(Object)result).setSchemeAndPath(scheme, path);
+			return result;
+		} catch (URISyntaxException e) {
+		}
+
+		return null;
+	}
+}
