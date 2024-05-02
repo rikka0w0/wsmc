024580e1f78dc8c92669cd8db38943af68ec2711
Mokocup <watashi.wa.yui.desu@gmail.com>
2024-05-02 15:48:45 +0700

    feat: Add SNI support for Cloudflare SSL Cert

--- src/main/java/wsmc/client/WebSocketClientHandler.java
+++ src/main/java/wsmc/client/WebSocketClientHandler.java
@@ -2,9 +2,14 @@ package wsmc.client;
 
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.Collections;
 
+import javax.net.ssl.SNIHostName;
+import javax.net.ssl.SSLEngine;
 import javax.net.ssl.SSLException;
+import javax.net.ssl.SSLParameters;
 
+import io.netty.buffer.ByteBufAllocator;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelPipeline;
@@ -21,6 +26,7 @@ import io.netty.handler.codec.http.websocketx.WebSocketVersion;
 import io.netty.handler.codec.http.websocketx.extensions.compression.WebSocketClientCompressionHandler;
 import io.netty.handler.ssl.SslContext;
 import io.netty.handler.ssl.SslContextBuilder;
+import io.netty.handler.ssl.SslHandler;
 import io.netty.handler.ssl.util.InsecureTrustManagerFactory;
 import io.netty.util.CharsetUtil;
 
@@ -72,28 +78,36 @@ public class WebSocketClientHandler extends WebSocketHandler {
 	}
 
 	public static void hookPipeline(ChannelPipeline pipeline, IWebSocketServerAddress wsInfo) {
-        final WebSocketClientHandler handler = WebSocketClientHandler.fromServerData(wsInfo);
+		final WebSocketClientHandler handler = WebSocketClientHandler.fromServerData(wsInfo);
 
-        if (handler != null) {
-        	pipeline.addAfter("timeout", "WsmcHttpClient", new HttpClientCodec());
-        	pipeline.addAfter("WsmcHttpClient", "WsmcHttpAggregator", new HttpObjectAggregator(8192));
-        	pipeline.addAfter("WsmcHttpAggregator", "WsmcCompressionHandler", WebSocketClientCompressionHandler.INSTANCE);
-        	pipeline.addAfter("WsmcCompressionHandler", "WsmcWebSocketClientHandler", handler);
+		if (handler != null) {
+			pipeline.addAfter("timeout", "WsmcHttpClient", new HttpClientCodec());
+			pipeline.addAfter("WsmcHttpClient", "WsmcHttpAggregator", new HttpObjectAggregator(8192*4));
+			pipeline.addAfter("WsmcHttpAggregator", "WsmcCompressionHandler", WebSocketClientCompressionHandler.INSTANCE);
+			pipeline.addAfter("WsmcCompressionHandler", "WsmcWebSocketClientHandler", handler);
 
-        	SslContext sslCtx = null;
-        	if ("wss".equalsIgnoreCase(wsInfo.getScheme())) {
-        		try {
-					sslCtx = SslContextBuilder.forClient()
-					        .trustManager(InsecureTrustManagerFactory.INSTANCE).build();
+			if ("wss".equalsIgnoreCase(wsInfo.getScheme())) {
+				try {
+					SslContext sslCtx = SslContextBuilder.forClient()
+							.trustManager(InsecureTrustManagerFactory.INSTANCE).build();
+
+					// SSL Parameters to set SNI TLS Extension
+					SSLParameters sslParameters = new SSLParameters();
+					sslParameters.setServerNames(Collections.singletonList(new SNIHostName(wsInfo.asServerAddress().getHost())));
+
+					// SSLEngine with SSL Parameters for SNI
+					SSLEngine sslEngine = sslCtx.newEngine(ByteBufAllocator.DEFAULT);
+					sslEngine.setSSLParameters(sslParameters);
+
+					// SSL Handler
+					SslHandler sslHandler = new SslHandler(sslEngine);
+
+					pipeline.addAfter("timeout", "WsmcSslHandler", sslHandler);
 				} catch (SSLException e) {
 					e.printStackTrace();
 				}
-
-            	if (sslCtx != null) {
-            		pipeline.addAfter("timeout", "WsmcSslHandler", sslCtx.newHandler(null));
-            	}
-        	}
-        }
+			}
+		}
 	}
 
 	@Override
